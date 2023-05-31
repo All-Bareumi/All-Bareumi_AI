cb9be28d1366ade27f99f0b5352681f9182c355d
bigstar9906 <bigstar9906@naver.com>
2023-05-31 21:54:05 +0900

    test

--- Flask/main.py
+++ Flask/main.py
@@ -1,7 +1,7 @@
-import sys, os
+import sys, os, subprocess, platform
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from Wav2Lip import api_test
+from Wav2Lip import generate_lipsync
 
 from flask import Flask, request
 from flask_restx import Resource, Api
@@ -9,13 +9,16 @@ from flask_restx import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-@api.route('/test')
-class Test(Resource):
-    def get(self):
-        return{"message" : api_test.test_function("Test is Okay")}
+@api.route('/sentence/insert')
+class Sentence(Resource):
     def post(self):
-        name = request.json.get('name')
-        return {"message" : api_test.test_function(name)}
+        gender = request.json.get('gender')
+        input_text = request.json.get('input_text')
+        input_image = '../Wav2Lip/my_data/'+request.json.get('character')
+        out_path = request.json.get('out_path')
+        result = generate_lipsync.generate(gender,input_text,input_image,out_path);
+        return {"success" : result}
+        
         
 
 if __name__ == "__main__":
