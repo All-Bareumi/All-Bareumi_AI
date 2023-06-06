bbdee096ac7f081206f4458f4c2ff48c77b389c2
sksmslhy <tbwn0406@naver.com>
2023-06-06 17:17:47 +0900

    Update pro_eval.py

--- Pronounce_Evaluation/pro_eval.py
+++ Pronounce_Evaluation/pro_eval.py
@@ -1,26 +1,41 @@
 #-*- coding:utf-8 -*-
 import urllib3
 import json
-import base64
+import scipy.io.wavfile as wav
+import numpy as np
+import librosa
+
 #openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Pronunciation" # 영어
 openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/PronunciationKor" # 한국어
 
-accessKey = "MyKey"
+accessKey = ""
 audioFilePath = "./data/hayun_wrong_16.wav"
-languageCode = "korean"
 script = "안녕하세요"
 
-file = open(audioFilePath, "rb")
-audioContents = base64.b64encode(file.read()).decode("utf8")
-file.close()
+def change_sample_rate(input_file, target_sample_rate):
+    # 오디오 파일 읽기
+    sample_rate, audio = wav.read(input_file)
+    
+    # 샘플링 주파수 변경
+    converted_audio = librosa.resample(audio.astype(float), sample_rate, target_sample_rate)
+    
+    return converted_audio
 
+def pro_eval(audioFilePath, script):
 
-def pro_eval():
+    languageCode = "korean"
+
+    #file = open(audioFilePath, "rb")
+    #audioContents = base64.b64encode(file.read()).decode("utf8")
+    #file.close()
+
+    resampling_audio = change_sample_rate(audioFilePath, 16000)
+
     requestJson = {   
         "argument": {
             "language_code": languageCode,
             "script": script,
-            "audio": audioContents
+            "audio": resampling_audio
         }
     }
 
@@ -40,5 +55,6 @@ def pro_eval():
     return pro_eval_score
 
 if __name__ == "__main__":
-    pro_eval()
+    pro_eval_score = pro_eval(audioFilePath, script)
+    print(pro_eval_score)
 
